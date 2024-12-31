902621addfd7a97d29038a4377a0a41f87f8da6c
HarshDhiman <142768740+harshDhiman3344@users.noreply.github.com>
2024-12-31 17:33:18 +0530

    rearranged the sections

--- src/App.jsx
+++ src/App.jsx
@@ -10,12 +10,16 @@ function App() {
       <Navbar/>
       <Home/>
       </section>
-      <section id="community"><h1>Community Hub</h1></section>
-      <section id="learning">Blogs and research</section>
-      <section id='news'>News and Events</section>
-      <section id='learning'>Interactive Learning</section>
       <section id="about">Society Information</section>
-    
+      
+      <section id="learning">Blogs and research</section>
+
+      <section id="community"><h1>Community Hub</h1></section>
+      
+      <section id='news'>News and Events</section>
+
+      <section id='learning'>Interactive Learning</section>
+  
     </>
   )
 }
