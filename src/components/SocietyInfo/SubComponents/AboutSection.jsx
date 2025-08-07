6bb4ea37845380df4ee0ea268e5987780ca21b70
HarshDhiman <142768740+harshDhiman3344@users.noreply.github.com>
2025-08-07 23:57:02 +0530

    vercel fix 2

--- src/components/SocietyInfo/SubComponents/AboutSection.jsx
+++ src/components/SocietyInfo/SubComponents/AboutSection.jsx
@@ -3,10 +3,10 @@ import { motion } from "framer-motion";
 import "./aboutSection.css"; // CSS ko import karna mat bhulna
 
 // Directly importing images
-import img1 from "./images/1.jpg";
-import img2 from "./images/2.jpg";
-import img3 from "./images/3.jpg";
-import img4 from "./images/4.jpg";
+import img1 from "./Images/1.jpg";
+import img2 from "./Images/2.jpg";
+import img3 from "./Images/3.jpg";
+import img4 from "./Images/4.jpg";
 
 const items = [
   { title: "a", image: img1 },
